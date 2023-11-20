372e6b38e9d0f0e68b388858050a714f8937342a
Pro-shanto06 <proshantosaha1999@gmail.com>
2023-11-20 17:08:06 +0600

    this project is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { GotQuotesComponent } from './got-quotes/got-quotes.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'contact-us',
+    component: ContactUsComponent
+  },
+  {
+    path:'got-quotes',
+    component: GotQuotesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
